a99834b159921437f75dc7067de3f52615575541
unknown <jocarosa@github.com>
2020-05-08 19:14:16 -0400

    fix: selecting pokemon list

--- src/ui/center/pokemon-list-cmp.js
+++ src/ui/center/pokemon-list-cmp.js
@@ -13,7 +13,10 @@ import { fetchPokemonData } from '../../services/fetchPokemonData';
 import { Navbar,Nav,DropdownButton } from 'react-bootstrap';
 import './style.css';
 import styled, { keyframes } from 'styled-components';
-import { bounceInLeft } from 'react-animations';
+
+//animations
+import { bounceInLeft, fadeOut } from 'react-animations';
+import {Animated} from "react-animated-css";
 
 //redux
 import {connect} from 'react-redux';
@@ -40,22 +43,27 @@ const mapStateProps = state => {
 
 const CONTAINERPOKE = styled.div`
   animation: 1s  ${keyframes `${bounceInLeft}`}
-`
+`;
 
+const MESSAGEEXCEDED = styled.div`
+    color:red;
+    animation: 12s  ${keyframes `${fadeOut}`};
+`;
 
 class List extends Component{
 
     state = {
         pokemonList : [],
+        pokemonListTwo : [],
         allPokemons: 150,
         limit:150,
         offSet:0,
         description:null,
         chain:null,
-       // pokemonsSelected:[],
-        listSelected:[]
+        listaSeleccionados:0,
+        exceedErrorVisible:false
     }
-   // pokemonsSelected=[];
+    showErrorExClass = false;
     styleHover;
 
     constructor(props){
@@ -64,7 +72,6 @@ class List extends Component{
        this.handleModalShow = this.handleModalShow.bind(this);
        this.handleModalHide = this.handleModalHide.bind(this);
         this.handleSelectedPokemon = this.handleSelectedPokemon.bind(this);
-
     }
 
 
@@ -137,8 +144,8 @@ class List extends Component{
     }
 
     handlePokemonApi = (url)=>{
-        let pokemonList =null;
-        let pokemonListTwo=null;
+        let pokemonList =[];
+        let pokemonListTwo=[];
         this.setState({pokemonList,listFetched:false})
 
         fetch(url).then(res=>res.json()).then(
@@ -149,34 +156,73 @@ class List extends Component{
                // this.props.addPokemonList(pokemonList);
            }
        )
-
     }
 
     handleSelectedPokemon = (pokemonSelected)=>{
-            
-            const isChecked = pokemonSelected.checked;
-            this.setState({
-                pokemonList : this.state.pokemonList.map(poke=>
-                    poke.name === pokemonSelected.pokemonName ? {...poke, isChecked}:poke
-                ),
-                pokemonListTwo : this.state.pokemonListTwo.map(poke=>
-                    poke.name === pokemonSelected.pokemonName ? {...poke, isChecked}:poke
-                )
-            },()=>{
-                
-               let list = this.state.pokemonListTwo.filter(s=>{
-                    if(s.hasOwnProperty("isChecked")){
-                        return s.isChecked
-                    }
-                })
-
-                    this.setState({
-                        listSelected:list,
-                        showNextButton: list.length == 6
-                    })
-            })
         
+        const isChecked = pokemonSelected.checked;
+        
+        if(this.state.listaSeleccionados == 6 &&  isChecked){
+            this.showErrorMessage();
+            return
+        }
+
+        if(isChecked){
+            this.setState(
+                (prev,p)=>({listaSeleccionados : prev.listaSeleccionados + 1}),
+                ()=>{
+                  
+                }
+            )
+        }else{
+            this.setState(
+                (prev,p)=>({listaSeleccionados : prev.listaSeleccionados - 1}),
+                ()=>{
+                    console.log(this.state.listaSeleccionados);
+                 }
+               )
+        }
+
+            
+        this.addPokemonCheckedToMainList(pokemonSelected);
     }   
+    
+
+    addPokemonCheckedToMainList=(pokemonSelected)=>{
+
+        const isChecked = pokemonSelected.checked;
+    
+        this.setState({
+            pokemonList : this.state.pokemonList.map(poke=>
+                poke.name === pokemonSelected.pokemonName ? {...poke, isChecked}:poke
+            ),
+            pokemonListTwo : this.state.pokemonListTwo.map(poke=>
+                poke.name === pokemonSelected.pokemonName ? {...poke, isChecked}:poke
+            )
+        },()=>{
+            this.toggleNextButton();    
+        })
+    }
+
+    toggleNextButton=()=>{
+        const cantidadSeleccionados = this.state.listaSeleccionados;
+        this.setState({
+            showNextButton: cantidadSeleccionados == 6
+        })
+    }
+
+    showErrorMessage=()=>{
+        this.setState({
+            exceedErrorVisible:true
+        })
+       setTimeout(()=>{
+        this.setState({
+            exceedErrorVisible:false
+        }) 
+       })
+       this.showErrorEx = true;
+    }
+
     getPokemonIndexFromList = (pokemonList,pokemonName)=>{
         const index = pokemonList.findIndex(
             SelectedPoke=>SelectedPoke.pokemonName === pokemonName
@@ -185,112 +231,159 @@ class List extends Component{
         return index;
     }
 
-    damePokemonElegidos = ()=>{
-        let pokemonChoose = [];
-        for(let ch=1;ch<=6;ch++){
-            if(typeof this.state.listSelected[ch-1]  !== "undefined"){
-                const pokemonName = this.state.listSelected[ch-1].name;
-                pokemonChoose.push(
-                <div className='containerPokeChoose'>
-                    <img className='styleImgPokeChoose' key={ch} src={`${urlNormal}${pokemonName}.png`}></img>
+    getImgsPokemonsSelected = ()=>{
+
+        let allPokeImgsSelected = [];
+        const seleccionados = this.getPokemonsSelectedistObject();
+
+        for(let index=1;index<=6;index++){
+            if(typeof seleccionados[index-1]  !== "undefined"){
+                
+                allPokeImgsSelected.push(
+                    this.getPokeImgSelected(index)
+                );
+                continue;
+            }
+            allPokeImgsSelected.push(
+                this.getDefaultImgBall(index)
+            )
+        }
+
+        return allPokeImgsSelected.map(s=>s);
+    }
+  
+    getPokemonsSelectedistObject(){
+        const seleccionados = this.state.pokemonListTwo.length>0 ?
+        this.state.pokemonListTwo.filter(poke=>{
+            return poke.isChecked
+        }):[];
+
+        return seleccionados;
+    }
+    getPokeImgSelected(index){
+        
+        const seleccionados = this.getPokemonsSelectedistObject();
+        const pokemonName = seleccionados[index-1].name;
+        return (
+            <div className='containerPokeChoose'>
+                    <img className='styleImgPokeChoose' key={index} 
+                    src={`${urlNormal}${pokemonName}.png`}></img>
                     <label onClick={()=>this.handleSelectedPokemon({
                         pokemonName,
                         checked:false
                     })}className='styleXChoose'>X</label> 
-                </div>    
-                );
-                continue;
-            }
-            pokemonChoose.push(
-            <div className='containerPokeChoose'>
-                <img key={ch} className='styleBallChooseTwo' src={require('../../img/pokeball.png')}></img>
-            </div>
-            )
-        }
-
-        return pokemonChoose.map(s=>s);
+            </div>    
+        )
     }
+    getDefaultImgBall=(index)=>{
+        return (
+            <div className='containerPokeChoose'>
+                <img key={index} className='styleBallChooseTwo' src={require('../../img/pokeball.png')}></img>
+            </div>
+        )
+    }
+    render(){
 
-        render(){
-
-            let pokemonListContainer = <h2>Loading...</h2>;
-           
-
-
-            if(this.state.listFetched){
-
-                const filterPokemonByNo = (thispokemon)=>{
-
-                    const pokemonNo = getPokemonNo(thispokemon);
-                    const isPokemonOfGeneration = pokemonNo <= this.state.limit;
-                    return isPokemonOfGeneration;
-                }
-                const getPokemonNo = (thispokemon)=>{
-                    const pokemonNo = thispokemon.url.split("/")[6];
-                    thispokemon["pokemonNo"] = parseInt(pokemonNo);
-                   return pokemonNo;
-                }
-                pokemonListContainer =  this.state.pokemonList.filter(p=>filterPokemonByNo(p))
-                .map((thispokemon,index)=>{
-                    const isChecked = this.state.pokemonList[index].isChecked || false;
-                    return (
-                     <PokemonItem 
-                     key={index}
-                     selectPokemon={(pokemonSelected)=>this.handleSelectedPokemon(pokemonSelected)} 
-                     isChecked = {isChecked}
-                     pokemonIn = {thispokemon}>
-                     </PokemonItem> 
-                     )
-                     
-                })
+        let pokemonListContainer = <h2>Loading...</h2>;
+        
 
 
+        if(this.state.listFetched){
+
+            const filterPokemonByNo = (thispokemon)=>{
+
+                const pokemonNo = getPokemonNo(thispokemon);
+                const isPokemonOfGeneration = pokemonNo <= this.state.limit;
+                return isPokemonOfGeneration;
             }
-
-
-            return(
-                <>
-                {/* 
-                    <Navbar className='sticky-nav' >
-                        <Navbar.Brand href="#home">Poke Arena</Navbar.Brand>
-                        <Nav className="mr-auto">
-                        <Search className='styleDropdown' onChange={this.handlePokemonSearch}/>
-                        { <DropdownButton variant="outline-danger" className='styleDropdown' id="dropdown-basic-button" title="Generation">
-                            <GenerationsList handleGetPokeByGeneration = {this.handleGetPokeByGeneration} />
-                        </DropdownButton> }
-                        </Nav>
-                    </Navbar> */}
-                    <div className='sticky-nav'>
-                        <Search onChange={this.handlePokemonSearch}/>
-                        <Steps></Steps>
-                        <div className='containerPokemonsChoosen'>
-                          {this.damePokemonElegidos()}
-                          <img className=
-                          {this.state.showNextButton?'showChooseSuccess':'hideChooseSuccess'} 
-                          src={require('../../img/chooseSuccess.png')}></img>
-                        </div>
-                    </div>
-                   
-                    
-                   <CONTAINERPOKE>
-                        <div className='containerPokemon' >
-                            {pokemonListContainer}
-                        </div>
-                    </CONTAINERPOKE>
-                    <BACKFORWARD steps={this.state}></BACKFORWARD>
+            const getPokemonNo = (thispokemon)=>{
+                const pokemonNo = thispokemon.url.split("/")[6];
+                thispokemon["pokemonNo"] = parseInt(pokemonNo);
+                return pokemonNo;   
+            }
+            pokemonListContainer =  this.state.pokemonList.filter(p=>filterPokemonByNo(p))
+            .map((thispokemon,index)=>{
                 
-                    <PokemonDetailModal
-                    chain = {this.state.chain}
-                    description = {this.state.description}
-                    types = {this.state.types}
-                    show={this.state.modalShow}
-                    onHide={()=>this.handleModalHide()} />
+                const isChecked = this.state.pokemonList[index].isChecked || false;
+                return (
+                    <PokemonItem 
+                    key={index}
+                    selectPokemon={(pokemonSelected)=>this.handleSelectedPokemon(pokemonSelected)} 
+                    isChecked = {isChecked}
+                    pokemonIn = {thispokemon}>
+                    </PokemonItem> 
+                    )
+                    
+            })
 
-                    {/* <Paginator config={this.state} handlePokemonApi={this.handlePokemonApi} /> */}
-                </>
-            )
 
         }
+
+
+        return(
+            <>
+            {/* 
+                <Navbar className='sticky-nav' >
+                    <Navbar.Brand href="#home">Poke Arena</Navbar.Brand>
+                    <Nav className="mr-auto">
+                    <Search className='styleDropdown' onChange={this.handlePokemonSearch}/>
+                    { <DropdownButton variant="outline-danger" className='styleDropdown' id="dropdown-basic-button" title="Generation">
+                        <GenerationsList handleGetPokeByGeneration = {this.handleGetPokeByGeneration} />
+                    </DropdownButton> }
+                    </Nav>
+                </Navbar> */}
+                                
+                <div className='sticky-nav'>
+                    <Search onChange={this.handlePokemonSearch}/>
+                    <Steps></Steps>
+                    <div className='containerPokemonsChoosen'>
+                        {this.getImgsPokemonsSelected()}
+                        <img className=
+                        {this.state.showNextButton?'showChooseSuccess':'hideChooseSuccess'} 
+                        src={require('../../img/chooseSuccess.png')}></img>
+                    
+                    <Animated 
+                    animationOutDuration={7000}
+                    animationOut="fadeOut" isVisible={
+                        this.state.exceedErrorVisible
+                    }>
+                        <div className={this.showErrorExClass?'showErrorMessage':'hideErrorMessage'}>
+                            <label>error</label>
+                        </div>
+                    </Animated>     
+                    
+                        
+                        {/* <MESSAGEEXCEDED 
+                        className={this.state.exceedErrorVisible?
+                        'showErrorMessage':'hideErrorMessage'}>
+                        You can choose only 6!
+                        </MESSAGEEXCEDED>  */}
+                        
+                    </div>
+    
+                    
+                </div>
+                
+    
+                <CONTAINERPOKE>
+                    <div className='containerPokemon' >
+                        {pokemonListContainer}
+                    </div>
+                </CONTAINERPOKE>
+                <BACKFORWARD steps={this.state}></BACKFORWARD>
+            
+                <PokemonDetailModal
+                chain = {this.state.chain}
+                description = {this.state.description}
+                types = {this.state.types}
+                show={this.state.modalShow}
+                onHide={()=>this.handleModalHide()} />
+
+                {/* <Paginator config={this.state} handlePokemonApi={this.handlePokemonApi} /> */}
+            </>
+        )
+
+    }
 }
 
 
@@ -340,24 +433,29 @@ class PokemonItem extends Component{
             const url=`${urlNormal}${pokemonName}.png`;
             this.setState({ hover: false,url })
          }
-         const selectPokemon =(pokemonName)=>{
-           this.pokemonSelected = !this.pokemonSelected;
-           this.props.selectPokemon({pokemonName,checked:this.pokemonSelected});
+         const selectPokemon =(pokemonName,checked)=>{
+           this.props.selectPokemon({pokemonName,checked:!checked});
          }
 
         return (
             
             <>
      
-             <div onClick={()=>selectPokemon(pokemonName)} className='itemPokemon'
+             <div className='itemPokemon'
                  style={ setStyleHover }
                  onMouseOver={ () => onHoverPokemon() }
                  onMouseOut={ () => onOutPokemon() } >
-                <div  className="checkbox custom" >
-                 <input onClick={()=>selectPokemon(pokemonName)} checked={this.props.isChecked}  id={pokemonName} className="css-checkbox" type="checkbox" />
+                <div  className="checkbox custom">
+                 <input 
+                  onClick={()=>selectPokemon(pokemonName,this.props.isChecked)}
+                   checked={this.props.isChecked}  
+                   id={pokemonName} className="css-checkbox" type="checkbox" />
                  <label htmlFor={pokemonName} className="css-label-red"></label> 
                 </div>
-                 <img alt='' className={this.props.isChecked?"stylePokemonChecked":"stylePokemonImg"} src={ this.state.hover?this.state.url : `${urlNormal}${pokemonName}.png` }></img>
+                 <img alt='' 
+                 className={this.props.isChecked?"stylePokemonChecked":"stylePokemonImg"} 
+                 src={ this.state.hover?this.state.url : `${urlNormal}${pokemonName}.png` }>
+                 </img>
              </div>
             
             </>
