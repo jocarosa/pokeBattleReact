ffcfc3f349942e3b7c8e90f22deb73606f100cb7
unknown <jocarosa@github.com>
2020-08-16 20:33:44 -0400

    fix: util_function

--- src/util/util_functions.js
+++ src/util/util_functions.js
@@ -1,3 +1,7 @@
+
+import React from 'react';
+import { itemsByPage , urlAnimated , urlNormal} from '../share';
+
 export function filterPokemonListByName ( pokemonName , state ){
 
     let pokemonList = state.pokemonListTwo;
@@ -15,4 +19,84 @@ export function  getPokemonNumber  ( thispokemon ) {
     const pokemonNo = thispokemon.url.split("/")[6];
     thispokemon["pokemonNo"] = parseInt(pokemonNo);
     return pokemonNo;   
+}
+
+export function  getPokemonsSelectedFromList(pokemonList){
+
+    let seleccionados = [];
+    
+    if( pokemonList.length > 0 ){
+        seleccionados = pokemonList.filter(poke=>{
+            return poke.isChecked
+        }); 
+    }
+
+    return seleccionados;
+}
+
+export function setPokemonSelectedToList(pokemonList , pokemonSelected){
+ 
+   const isChecked = pokemonSelected.checked;
+   const pokemonSelectedList = pokemonList.map( currentPokemon =>{
+
+        if(currentPokemon.name === pokemonSelected.pokemonName){
+            return {...currentPokemon, isChecked}
+        }else{
+            return currentPokemon;
+        }
+
+    });
+
+    return pokemonSelectedList;
+
+}
+
+export function getImgFromPokemonsSelectedList( pokemonList , showCloseButton ){
+
+    let allPokeImgsSelected = [];
+    const seleccionados = getPokemonsSelectedFromList( pokemonList );
+
+    for( let index=1;index<=6;index++ ){
+
+        if(typeof seleccionados[index-1]  !== "undefined"){
+      
+            const pokemonName = seleccionados[index-1].name;
+
+            let img = 
+
+                (<div key={index} className='containerPokeChoose'>
+                    <img alt="" className='styleImgPokeChoose' key={index} src={`${urlNormal}${pokemonName}.png`}></img>
+                    <label                   
+                        onClick = {
+                            ()=>this.handleSelectedPokemon({
+                            pokemonName,
+                            checked:false})
+                        } 
+                        className='styleXChoose'>
+                        <span className={showCloseButton?"showContainer":"hideContainer"}>X</span>
+                    </label> 
+                </div>   ) 
+
+
+           allPokeImgsSelected.push(img);
+          continue;                 
+       }   
+       
+       allPokeImgsSelected.push(getDefaultPokeBallImg(index));
+    
+    }            
+        
+    return allPokeImgsSelected.map(s=>s);
+    
+}
+
+
+
+export function getDefaultPokeBallImg(index){
+        
+        return (
+            <div key={index} className='containerPokeChoose'>
+                <img alt="" key={index} className='styleBallChooseTwo' src={require('../img/pokeball.png')}></img>
+            </div>
+        )    
 }
